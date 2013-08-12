00af468e02cd7bc77f2378a3af3be7bcf6838dd9
lsb <leebutterman@gmail.com>
2013-08-12 12:32:44 +0000

    bug fix: ports

--- turk-io-daemon.sh
+++ turk-io-daemon.sh
@@ -1,6 +1,6 @@
 while :
 do
-  curl -X POST -d '' localhost:4567/i
-  curl -X POST -d '' localhost:4567/o
+  curl -X POST -d '' localhost:6000/i
+  curl -X POST -d '' localhost:6000/o
   sleep 10
 done
