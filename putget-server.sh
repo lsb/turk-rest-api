922d6f0b977664072f4363c39652abfa30035688
lsb <leebutterman@gmail.com>
2013-10-27 00:09:30 +0000

    turk rest api is now on port 7000

--- putget-server.sh
+++ putget-server.sh
@@ -1,1 +1,1 @@
-ruby http.rb -p 6000 -e production >> stdout.log 2>> stderr.log
+ruby http.rb -p 7000 -e production >> stdout.log 2>> stderr.log
--- turk-io-daemon.sh
+++ turk-io-daemon.sh
@@ -1,6 +1,6 @@
 while :
 do
-  curl -X POST -d '' localhost:6000/i
-  curl -X POST -d '' localhost:6000/o
+  curl -X POST -d '' localhost:7000/i
+  curl -X POST -d '' localhost:7000/o
   sleep 10
 done
